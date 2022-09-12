0d4f20264f0ca1e5929ac59d0e5f255357cdcd5f
JOSP <josiptadic96@hotmail.com>
2022-09-12 22:23:52 +0200

    3.22: Lint configuration

--- index.js
+++ index.js
@@ -1,145 +1,146 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const morgan = require("morgan");
-require("dotenv").config();
-const Person = require("./models/person");
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const morgan = require('morgan')
+require('dotenv').config()
+const Person = require('./models/person')
 
 const requestLogger = (request, response, next) => {
-  console.log("Method:", request.method);
-  console.log("Path:  ", request.path);
-  console.log("Body:  ", request.body);
-  console.log("---");
-  next();
-};
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
 
-app.use(express.json());
-app.use(requestLogger);
-app.use(cors());
-app.use(express.static("build"));
+app.use(express.json())
+app.use(requestLogger)
+app.use(cors())
+app.use(express.static('build'))
 
 app.use(
-  morgan("tiny", {
-    skip: function (req, res) {
-      return req.method === "POST";
+  morgan('tiny', {
+    skip: function (req) {
+      return req.method === 'POST'
     },
   })
-);
+)
 
-morgan.token("body", (req, res) => req.body && JSON.stringify(req.body));
+morgan.token('body', (req) => req.body && JSON.stringify(req.body))
 
 app.use(
   morgan(
-    ":method :url :status :response-time ms - :res[content-lenght] :body - :req[content-lenght]",
+    ':method :url :status :response-time ms - :res[content-lenght] :body - :req[content-lenght]',
     {
-      skip: function (req, res) {
-        return req.method !== "POST";
+      skip: function (req) {
+        return req.method !== 'POST'
       },
     }
   )
-);
+)
 
-app.get("/api/persons", (req, res, next) => {
+app.get('/api/persons', (req, res, next) => {
   Person.find({})
     .then((persons) => {
-      res.json(persons);
+      res.json(persons)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.get("/info", (request, response) => {
-  const currentTime = new Date();
+app.get('/info', (request, response) => {
+  const currentTime = new Date()
   Person.count({}, function (err, count) {
     response.send(
       `<p>Phonebook has info for ${count} people</p><p>${currentTime.toString()}</p>`
-    );
-  });
-});
+    )
+  })
+})
 
-app.get("/api/persons/:id", (request, response, next) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then((person) => response.json(person))
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.delete("/api/persons/:id", (request, response, next) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
-    .then((result) => {
-      response.status(204).end();
+    .then(() => {
+      response.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.post("/api/persons", (request, response, next) => {
-  const body = request.body;
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
 
   const person = new Person({
     name: body.name,
     number: body.number,
-  });
+  })
 
   Person.findOne({ name: person.name })
     .then((result) => {
       if (result) {
-        throw new Error("SameName");
+        throw new Error('SameName')
       } else {
         person
           .save()
           .then((savedPerson) => {
-            response.json(savedPerson);
+            response.json(savedPerson)
           })
           .catch((error) => {
-            next(error);
-          });
+            next(error)
+          })
       }
     })
     .catch((error) => {
-      next(error);
-    });
-});
+      next(error)
+    })
+})
 
-app.put("/api/persons/:id", (request, response, next) => {
-  const body = request.body;
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
 
   const person = new Person({
     name: body.name,
     number: body.number,
-  });
+  })
 
   Person.findByIdAndUpdate(
     request.params.id,
     { $set: { name: person.name, number: person.number } },
-    { new: true, runValidators: true, context: "query" }
+    { new: true, runValidators: true, context: 'query' }
   )
     .then((updatedPerson) => {
-      response.json(updatedPerson);
+      response.json(updatedPerson)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
-  } else if (error.message === "SameName") {
-    return response.status(400).json({ error: "Can't POST same names" });
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  } else if (error.message === 'SameName') {
+    return response.status(400).json({ error: 'Can\'t POST same names' })
   }
 
-  next(error);
-};
+  next(error)
+}
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001;
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- mongo.js
+++ mongo.js
@@ -1,58 +1,59 @@
-const mongoose = require("mongoose");
+/* eslint-disable no-undef */
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
   console.log(
-    "Please provide the password as an argument: node mongo.js <password>"
-  );
-  process.exit(1);
+    'Please provide the password as an argument: node mongo.js <password>'
+  )
+  process.exit(1)
 }
 
-const password = process.argv[2];
-const url = `mongodb+srv://fsopen:${password}@cluster0.gmeokla.mongodb.net/phonebookApp?retryWrites=true&w=majority`;
+const password = process.argv[2]
+const url = `mongodb+srv://fsopen:${password}@cluster0.gmeokla.mongodb.net/phonebookApp?retryWrites=true&w=majority`
 
 const personSchema = new mongoose.Schema({
   id: Number,
   name: String,
   number: String,
-});
+})
 
-const Person = mongoose.model("Person", personSchema);
+const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 3) {
   mongoose
     .connect(url)
-    .then((result) => {
-      console.log(`phonebook:`);
+    .then(() => {
+      console.log('phonebook:')
 
       Person.find({}).then((result) => {
         result.forEach((person) => {
-          console.log(person.name, person.number);
-        });
-        mongoose.connection.close();
-      });
+          console.log(person.name, person.number)
+        })
+        mongoose.connection.close()
+      })
     })
-    .catch((err) => console.log(err));
+    .catch((err) => console.log(err))
 }
 
 if (process.argv.length > 4) {
   mongoose
     .connect(url)
-    .then((result) => {
-      console.log("connected");
+    .then(() => {
+      console.log('connected')
 
       const person = new Person({
         id: Math.floor(Math.random() * 10000),
         name: process.argv[3],
         number: process.argv[4],
-      });
+      })
 
-      return person.save();
+      return person.save()
     })
     .then(() => {
       console.log(
         `Added ${process.argv[3]} number ${process.argv[4]} to phonebook`
-      );
-      return mongoose.connection.close();
+      )
+      return mongoose.connection.close()
     })
-    .catch((err) => console.log(err));
+    .catch((err) => console.log(err))
 }
